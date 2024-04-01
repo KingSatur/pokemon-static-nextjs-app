07c3635597de935e0ded9e347ea2b644c090b267
Juan Campillo <codwithjuanda@gmail.com>
2024-04-01 08:08:00 -0500

    feat: update to next 14 and some other deps with vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false,
   images: {
-    // domains: ["raw.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
