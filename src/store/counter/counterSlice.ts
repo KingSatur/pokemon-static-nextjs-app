e4561d5ff69236efe310e6e7a6dbbf12b187878d
Juan Campillo <codwithjuanda@gmail.com>
2024-04-14 20:36:40 -0500

    feat: use counter slice on counter component

--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -2,22 +2,31 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
   count: number;
+  isReady: boolean;
 }
 
 const initialState: CounterState = {
   count: 0,
+  isReady: false,
 };
 
 const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
+    initCounter(state, action: PayloadAction<number>) {
+      if (state.isReady) return;
+
+      state.count = action.payload;
+      state.isReady = true;
+    },
     add(state, action: PayloadAction<number>) {
       state.count += action.payload;
     },
     substract(state, action: PayloadAction<number>) {
       if (state.count - action.payload < 0) {
         state.count = 0;
+        return;
       }
       state.count -= action.payload;
     },
@@ -27,6 +36,7 @@ const counterSlice = createSlice({
   },
 });
 
-export const { add, resetCounter, substract } = counterSlice.actions;
+export const { add, resetCounter, substract, initCounter } =
+  counterSlice.actions;
 
 export default counterSlice.reducer;
