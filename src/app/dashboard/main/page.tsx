5d27da74245b6c43502f7e67cc92b168bb662d94
Juan Campillo <codwithjuanda@gmail.com>
2024-04-14 20:05:38 -0500

    feat: adding setup for redux and redux-toolkit

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,7 +1,11 @@
+import { SimpleWidget } from "@/src/components";
+
 export default function MainPage() {
   return (
     <div className="p-2 flex flex-col items-center justify-center w-full h-full">
-      <span>Main page</span>
+      <div className="flex flex-wrap p-2">
+        <SimpleWidget />
+      </div>
     </div>
   );
 }
